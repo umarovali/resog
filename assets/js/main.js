396f550235886dc7a2d8c73ab3b63b46f330e6c2
umarovali <umarovali.345@gmail.com>
2026-04-26 11:42:49 +0600

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,144 @@
-const swiper = new Swiper('.Reviews', {
-  slidesPerView: 4,
+const muswiper = new Swiper(".swiper_comment", {
+  slidesPerView: "auto",
   spaceBetween: 20,
-  loop: true,
+  centeredSlides: false,
+  grabCursor: true,
 
   navigation: {
-    nextEl: '.next-review',
-    prevEl: '.prev-review',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+  slidesPerGroup: 1,
+});
 
-  breakpoints: {
-    320: {
-      slidesPerView: 1.2,
-    },
-    768: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-    1400: {
-      slidesPerView: 4,
+
+const videoContainers = document.querySelectorAll('.connection-swiper-video, .connection_video');
+
+videoContainers.forEach(container => {
+  const video = container.querySelector('video');
+  const button = container.querySelector('.connection_play_pause');
+  const icon = button.querySelector('i');
+
+  button.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+      icon.classList.remove('fa-play');
+      icon.classList.add('fa-pause');
+      button.style.opacity = '0.5';
+    } else {
+      video.pause();
+      icon.classList.remove('fa-pause');
+      icon.classList.add('fa-play');
+      button.style.opacity = '1';
     }
-  }
+  });
+
+  video.addEventListener('ended', () => {
+    icon.classList.remove('fa-pause');
+    icon.classList.add('fa-play');
+    button.style.opacity = '1';
+  });
 });
 
+let connectionSwiper;
 
-new Swiper(".reviewsSwiper", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  loop: true,
+function initSwiper() {
+  const screenWidth = window.innerWidth;
 
-  navigation: {
-    nextEl: ".rev-next",
-    prevEl: ".rev-prev",
-  },
-
-  breakpoints: {
-    320: { slidesPerView: 1 },
-    640: { slidesPerView: 2 },
-    1024: { slidesPerView: 4 }
+  if (screenWidth <= 400 && !connectionSwiper) {
+    connectionSwiper = new Swiper('.connection-swiper', {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+      loop: false,
+      observer: true,
+      observeParents: true,
+      pagination: {
+        el: '.connection-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
   }
-});
+  else if (screenWidth > 400 && connectionSwiper) {
+    connectionSwiper.destroy(true, true);
+    connectionSwiper = undefined;
+  }
+}
+
+initSwiper();
+
+window.addEventListener('resize', initSwiper);
+
+// const swiper = new Swiper('.Reviews', {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   loop: true,
+
+//   navigation: {
+//     nextEl: '.next-review',
+//     prevEl: '.prev-review',
+//   },
+
+//   pagination: {
+//     el: '.swiper-pagination',
+//     clickable: true,
+//   },
+
+//   breakpoints: {
+//     320: {
+//       slidesPerView: 1.2,
+//     },
+//     768: {
+//       slidesPerView: 2,
+//     },
+//     1024: {
+//       slidesPerView: 3,
+//     },
+//     1400: {
+//       slidesPerView: 4,
+//     }
+//   }
+// });
+
+
+// new Swiper(".reviewsSwiper", {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   loop: true,
+
+//   navigation: {
+//     nextEl: ".rev-next",
+//     prevEl: ".rev-prev",
+//   },
+
+//   breakpoints: {
+//     320: { slidesPerView: 1 },
+//     640: { slidesPerView: 2 },
+//     1024: { slidesPerView: 4 }
+//   }
+// });
+
+
+
+
+
+// const container = document.querySelector(".scroll-content");
+
+// let scrollAmount = 0;
+
+// function autoScroll() {
+//   scrollAmount += 0.5;
+//   container.style.transform = `translateX(-${scrollAmount}px)`;
+
+//   if (scrollAmount >= container.scrollWidth / 2) {
+//     scrollAmount = 0;
+//   }
+
+//   requestAnimationFrame(autoScroll);
+// }
+
+// autoScroll();
